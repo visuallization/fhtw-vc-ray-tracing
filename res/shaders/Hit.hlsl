4d26f3e2df989d057a5ce8b4ccb330ecf39af97d
visuallisation <florentin.rieger@gmail.com>
2022-05-26 16:24:22 +0200

    feat(constant buffers): demomstrate a diffetent memory layout

--- res/shaders/Hit.hlsl
+++ res/shaders/Hit.hlsl
@@ -8,12 +8,16 @@ struct STriVertex
 
 StructuredBuffer<STriVertex> BTriVertex : register(t0);
 
+struct MyStructColor
+{
+	float4 a;
+	float4 b;
+	float4 c;
+};
 
 cbuffer Colors : register(b0)
 {
-	float3 A[3];
-	float3 B[3];
-	float3 C[3];
+	MyStructColor Tint[3];
 }
 
 [shader("closesthit")] 
@@ -22,13 +26,8 @@ void ClosestHit(inout HitInfo payload, Attributes attrib)
 	float3 barycentrics = float3(1.f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
 	
 	int instanceID = InstanceID();
-
-	float3 hitColor = float3(0.6, 0.7, 0.6);
-	// Shade only the first 3 instances (triangles)
-	if (instanceID < 3)
-	{
-		hitColor = A[instanceID] * barycentrics.x + B[instanceID] * barycentrics.y + C[instanceID] * barycentrics.z;
-	}
 	
+	float3 hitColor = Tint[instanceID].a * barycentrics.x + Tint[instanceID].b * barycentrics.y + Tint[instanceID].c * barycentrics.z;
+
 	payload.colorAndDistance = float4(hitColor, RayTCurrent());
 }
